02504a19fb52e3475d3a04b6e9b989096f40aa65
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-07-23 19:49:27 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -65,14 +65,16 @@ end
 
 
 def adding_matz
-add the following information to the top level of programmer_hash
 :yukihiro_matsumoto => {
   :known_for => "Ruby",
     :languages => ["LISP, C"]
 }
 
 	programmer_hash =
- 		{
+ 		{   :yukihiro_matsumoto => {
+          :known_for => "Ruby",
+          :languages => ["LISP, C"]
+}
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -89,17 +91,14 @@ add the following information to the top level of programmer_hash
 
   programmer_hash[:yukihiro_matsumoto] = {
     known_for: "Ruby",
-    languages:
-      ["LISP", "C"]
+    languages: ["LISP", "C"]
     }
     
   programmer_hash
-    
-    
-# Here is the learn.co way of doing it.    
-# programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
-# programmer_hash
 end
+
+
+
 
 adding_matz
 
@@ -119,7 +118,9 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable.
+     
+     
+     
      alans_new_info = "GUI"
      
   programmer_hash[:alan_kay][:known_for] = "GUI"
