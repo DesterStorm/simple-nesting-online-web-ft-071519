b79859aae391cc46c10e93d06fb0c80212ba920b
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-07-23 19:52:01 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -72,10 +72,10 @@ def adding_matz
 
 	programmer_hash =
  		{   
-#       :yukihiro_matsumoto => {
-#           :known_for => "Ruby",
-#           :languages => ["LISP, C"]
-# }
+      :yukihiro_matsumoto => {
+          :known_for => "Ruby",
+          :languages => ["LISP, C"]
+}
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -90,11 +90,11 @@ def adding_matz
         }
      }
 
-  programmer_hash[:yukihiro_matsumoto] = {
-    known_for: "Ruby",
-    languages: ["LISP", "C"]
-    }
-  end
+  # programmer_hash[:yukihiro_matsumoto] = {
+  #   known_for: "Ruby",
+  #   languages: ["LISP", "C"]
+  #   }
+    
   programmer_hash
 end
 
